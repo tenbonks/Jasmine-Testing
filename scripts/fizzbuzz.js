b6d51ad6b97bfd49e273324619c85b05a4c5705e
Ben Tonks <thebentonks@gmail.com>
2019-08-27 12:47:48 +0100

    Added for loop

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -1,6 +1,11 @@
 function fizzBuzz (number) {
+    
     if (typeof(number) == "number" && (number % 15 == 0)) {
+        if (number > 0){
         return "Fizz-Buzz";
+        } else {
+            return number;
+        }
     } else if (typeof(number) == "number" && (number % 3 == 0)) {
         return "Fizz";
     } else if (typeof(number) == "number" && (number % 5 == 0)) {
@@ -10,4 +15,11 @@ function fizzBuzz (number) {
     } else {
         alert ("Error!");
     };
-}
+
+};
+
+for (let i=0; i <= 100; i++) {
+    console.log(fizzBuzz(i))
+};
+
+ 
